ad73524c0c8c4ea594a594e3eea57d2765c12002
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-06-05 11:56:34 -0700

    add build

---
+++ build/precache-manifest.df44e49054ec4157601345c8f8d506c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fda608e810f24ef869f62b18f30a13d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7bbba1c92f06c5b06e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "88358ea1db530119b0bf",
+    "url": "/static/css/main.1876a5d0.chunk.css"
+  },
+  {
+    "revision": "9b7bbba1c92f06c5b06e",
+    "url": "/static/js/2.15c13364.chunk.js"
+  },
+  {
+    "revision": "88358ea1db530119b0bf",
+    "url": "/static/js/main.a5ee65ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
